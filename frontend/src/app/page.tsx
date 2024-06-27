64f07c456f85541a4b347631a12e44403490d02b
rabindrarajawat <rabindra.rajawat@gmail.com>
2024-06-27 14:03:53 +0530

    signup page create and fix routing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,71 +1,149 @@
+// "use client";
+// import Image from "next/image";
+// import styles from "./page.module.css";
+// import { Theme } from "@/context/ThemeContext/Theme.context";
+
+// import { useThemeContext } from "@/context/ThemeContext/Theme.provider";
+// import Header from "@/components/Header/Header";
+// import Footer from "@/components/Footer/Footer";
+// import SubMenu from "@/components/SubMenu/SubMenu";
+// import { useState } from "react";
+// import LoginForm from "@/components/Login/Login";
+// import { MarginTrading } from "@/components/MarginTrading/MarginTrading";
+// import { FuturesTrading } from "@/components/FuturesTrading/FuturesTrading";
+// import Head from "next/head";
+// import Spot from "@/components/spotTradingm/Spot";
+// // Assuming Deposit component is imported correctly
+// import Deposit from "@/components/Deposit/Deposit";
+
+// export default function Home() {
+//   const { theme } = useThemeContext();
+//   const isLightTheme = theme === Theme.LIGHT;
+//   const [activeMenu, setActiveMenu] = useState("Spot Trading");
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+//   return (
+//     <>
+//       <main>
+//         <Head>
+//           <link
+//             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+//             rel="stylesheet"
+//           />
+//         </Head>
+        
+//         <div>
+//           {!isLoggedIn ? (
+//             <LoginForm onLogin={() => setIsLoggedIn(true)} />
+//           ) : (
+//             <>
+//               <Header />
+//               <Spot activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
+//               <SubMenu
+//                 activeMenu={activeMenu}
+//                 setActiveMenu={setActiveMenu}
+//                 activePerformance={""}
+//                 setActivePerformance={() => {}}
+//               /> 
+//               <MarginTrading
+//                 activeMenu={activeMenu}
+//                 setActiveMenu={setActiveMenu}
+//                 activePerformance={""}
+//                 setActivePerformance={() => {}}
+//               />
+//               <FuturesTrading
+//                 activeMenu={activeMenu}
+//                 setActiveMenu={setActiveMenu}
+//                 activePerformance={""}
+//                 setActivePerformance={() => {}}
+//               /> 
+//               <Deposit
+//                 activeMenu={activeMenu}
+//                 setActiveMenu={setActiveMenu}
+//               />
+//               {/* <Footer /> */}
+//             </>
+//           )}
+//         </div>
+//       </main>
+//     </>
+//   );
+// }
+
+
+
+
+
 "use client";
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Theme } from "@/context/ThemeContext/Theme.context";
+import React, { useState, FormEvent } from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import styles from './page.module.css';
+import { useRouter } from 'next/navigation';
 
-import { useThemeContext } from "@/context/ThemeContext/Theme.provider";
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
-import SubMenu from "@/components/SubMenu/SubMenu";
-import { useState } from "react";
-import LoginForm from "@/components/Login/Login";
-import { MarginTrading } from "@/components/MarginTrading/MarginTrading";
-import { FuturesTrading } from "@/components/FuturesTrading/FuturesTrading";
-import Head from "next/head";
-import Spot from "@/components/spotTradingm/Spot";
-// Assuming Deposit component is imported correctly
-import Deposit from "@/components/Deposit/Deposit";
+const Home: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-export default function Home() {
-  const { theme } = useThemeContext();
-  const isLightTheme = theme === Theme.LIGHT;
-  const [activeMenu, setActiveMenu] = useState("Spot Trading");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
+
+  const handleLogin = (e: FormEvent) => {
+    e.preventDefault();
+    if (username && password) {
+      console.log(`Username: ${username}, Password: ${password}`);
+      // Simulating an onLogin action
+      router.push("/home");
+    } else {
+      // Optionally, display an error message or handle empty fields
+      console.log("Please fill in both username and password.");
+    }
+  };
 
   return (
-    <>
-      <main>
-        <Head>
-          <link
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-            rel="stylesheet"
-          />
-        </Head>
-        
-        <div>
-          {!isLoggedIn ? (
-            <LoginForm onLogin={() => setIsLoggedIn(true)} />
-          ) : (
-            <>
-              <Header />
-              <Spot activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
-              <SubMenu
-                activeMenu={activeMenu}
-                setActiveMenu={setActiveMenu}
-                activePerformance={""}
-                setActivePerformance={() => {}}
-              /> 
-              <MarginTrading
-                activeMenu={activeMenu}
-                setActiveMenu={setActiveMenu}
-                activePerformance={""}
-                setActivePerformance={() => {}}
-              />
-              <FuturesTrading
-                activeMenu={activeMenu}
-                setActiveMenu={setActiveMenu}
-                activePerformance={""}
-                setActivePerformance={() => {}}
-              /> 
-              <Deposit
-                activeMenu={activeMenu}
-                setActiveMenu={setActiveMenu}
-              />
-              {/* <Footer /> */}
-            </>
-          )}
-        </div>
-      </main>
-    </>
+    <div className={styles.container}>
+      <div className={styles.formWrapper}>
+        <h2>Login</h2>
+        <form className={styles.formContent} onSubmit={handleLogin}>
+          <div className={styles.inputBox}>
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <i className={`bi bi-person-fill ${styles.icon}`}></i>
+          </div>
+
+          <div className={styles.inputBox}>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <i className={`bi bi-lock-fill ${styles.icon}`}></i>
+          </div>
+
+          <div className={styles.rememberForget}>
+            <label>
+              <input type="checkbox" />
+              Remember me
+            </label>
+            <a href="#">Forget password?</a>
+          </div>
+          <div className={styles.buttonStyle}>
+            <button type="submit">Login</button>
+          </div>
+          <div className={styles.registerLink}>
+            <p>
+              Don't have an account? <a href="/signup">Sign Up</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
   );
-}
+};
+
+export default Home;
--- frontend/src/app/signup/page.js
+++ frontend/src/app/signup/page.js
@@ -1,39 +1,48 @@
-import React, { useState, FormEvent } from "react";
+"use client";
+import React, { useState } from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import styles from './signup.module.css';
+import { useRouter } from "next/navigation";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-// Prop type for the LoginForm component
-interface SignupFormProps {
-  onSignup: () => void;
-}
 
-const SignupForm: React.FC<SignupFormProps> = ({ onSignup }) => {
-  const [name, setname] = useState("");
+const Signup = () => {
+  const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [confirmpassword, setconfirmPassword] = useState("");
 
-  const handleLogin = (e: FormEvent) => {
+  const router = useRouter();
+
+  const createButton = (e) => {
     e.preventDefault();
-    console.log(`Username: ${username}, Password: ${password}`);
-
-    onSignup();
+    if (name && username && password && email && confirmPassword) {
+      if (password !== confirmPassword) {
+        toast.error("Password and confirm password do not match.");
+      } else {
+        // All fields are filled and passwords match, navigate to home page
+        router.push("/home");
+        toast.success("Account created successfully!");
+      }
+    } else {
+      toast.error("Please fill in all fields.");
+    }
   };
 
   return (
     <div className={styles.container}>
+      <ToastContainer position="top-right" autoClose={3000} />
       <div className={styles.formWrapper}>
-        <h2>Login</h2>
-        <form className={styles.formContent} onSubmit={handleLogin}>
-         
-         
-        <div className={styles.inputBox}>
+        <h2>Signup</h2>
+        <form className={styles.formContent} onSubmit={createButton}>
+          <div className={styles.inputBox}>
             <input
               type="text"
               placeholder="Name"
               value={name}
-              onChange={(e) => setname(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
               required
             />
             <i className={`bi bi-person-fill ${styles.icon}`}></i>
@@ -58,7 +67,8 @@ const SignupForm: React.FC<SignupFormProps> = ({ onSignup }) => {
               onChange={(e) => setEmail(e.target.value)}
               required
             />
- <i className={`bi bi-envelope-fill ${styles.icon}`}></i>          </div>
+            <i className={`bi bi-envelope-fill ${styles.icon}`}></i>
+          </div>
 
           <div className={styles.inputBox}>
             <input
@@ -70,14 +80,13 @@ const SignupForm: React.FC<SignupFormProps> = ({ onSignup }) => {
             />
             <i className={`bi bi-lock-fill ${styles.icon}`}></i>
           </div>
-          
-          
+
           <div className={styles.inputBox}>
             <input
               type="password"
               placeholder="Confirm password"
-              value={confirmpassword}
-              onChange={(e) => setconfirmPassword(e.target.value)}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
               required
             />
             <i className={`bi bi-lock-fill ${styles.icon}`}></i>
@@ -95,7 +104,7 @@ const SignupForm: React.FC<SignupFormProps> = ({ onSignup }) => {
           </div>
           <div className={styles.registerLink}>
             <p>
-              You have an account? <a href="#">Login Here</a>
+              You have an account? <a href="/">Login Here</a>
             </p>
           </div>
         </form>
@@ -104,4 +113,4 @@ const SignupForm: React.FC<SignupFormProps> = ({ onSignup }) => {
   );
 };
 
-export default SignupForm;
+export default Signup;
